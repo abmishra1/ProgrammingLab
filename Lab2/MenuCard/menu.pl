dc860331a9e464eb15b16735b6f93cdf18c74d97
Nitin Kedia <nitinkedia7@gmail.com>
2019-10-18 14:23:17 +0530

    Working Q2 code, fix false written after each query

--- Lab2/MenuCard/menu.pl
+++ Lab2/MenuCard/menu.pl
@@ -23,42 +23,44 @@ desert("Malai Sandwich", 30, 1).
 desert("Rasmalai", 10, 2).
 
 take_starter(_, 6, [], _).
-take_starter(_, 6, CL, TL) :-
-    append(CL, TL, CL),
-    writeln(CL),
+take_starter(_, 6, CL, PL) :-
+    CL \= [],
+    append(PL, CL, TL),
+    writeln(TL),
     fail.
 
-take_starter(N, I, CL, TL) :-
+take_starter(N, I, CL, PL) :-
     starter(Name, NV, I),
     N >= NV,
     N1 is N - NV,
-    take_starter(N1, I, [Name|CL], TL).
+    take_starter(N1, I, [Name|CL], PL).
     
-take_starter(N, I, CL, TL) :-
+take_starter(N, I, CL, PL) :-
     I < 6,
     J is I + 1,
-    take_starter(N, J, CL, TL).    
+    take_starter(N, J, CL, PL).    
 
-select_starter(N, TL) :- take_starter(N, 0, [], TL).
+select_starter(N, PL) :- take_starter(N, 0, [], PL).
 
 take_desert(_, 3, [], _).
-take_desert(_, 3, CL, TL) :-
-    append(CL, TL, TL),
-    writeln(CL),
-    !,fail.
+take_desert(_, 3, CL, PL) :-
+    CL \= [],
+    append(PL, CL, TL),
+    writeln(TL),
+    fail.
 
-take_desert(N, I, CL, TL) :-
+take_desert(N, I, CL, PL) :-
     desert(Name, NV, I),
     N >= NV,
     N1 is N - NV,
-    take_desert(N1, I, [Name|CL], TL).
+    take_desert(N1, I, [Name|CL], PL).
     
-take_desert(N, I, CL, TL) :-
+take_desert(N, I, CL, PL) :-
     I < 3,
     J is I + 1,
-    take_desert(N, J, CL, TL).    
+    take_desert(N, J, CL, PL).    
 
-select_desert(N, TL) :- take_desert(N, 0, [], TL).
+select_desert(N, PL) :- take_desert(N, 0, [], PL).
 
 satisfy_hungry() :-
     starter(S, _, _),
@@ -69,14 +71,36 @@ satisfy_hungry() :-
     fail.
 
 satisfy_not_so_hungry(X) :-
+    % writeln("inhere"),
     main_dish(Name, NV),
     N1 is 80 - NV,
+    writeln(Name),
+    writeln(N1),
+    % select_starter(N1,[Name]),
     ( X = 1 -> select_starter(N1,[Name]) ; select_desert(N1,[Name])),
     fail.
-    
+
+satisfy_diet(X,Y,Z):-
+    (X = 1, satisfy_starter_diet());
+    (Y = 1, satisfy_main_dish_diet());
+    (Z = 1, satisfy_desert_diet()).
+
+satisfy_starter_diet() :-
+    select_starter(40, []),
+    fail.
+
+satisfy_desert_diet() :-
+    select_desert(40, []),
+    fail.
+
+satisfy_main_dish_diet() :-
+    main_dish(Name, N),
+    N =< 40,
+    writeln([Name]),
+    fail.
+
 find_items(Status, X, Y, Z) :-
     menu(Status, X, Y, Z),
-    (Status = hungry, satisfy_hungry());
-    (Status = not_so_hungry, satisfy_not_so_hungry(X)).
-
-
+    ((Status = hungry, satisfy_hungry());
+    (Status = not_so_hungry, satisfy_not_so_hungry(X));
+    (Status = diet, satisfy_diet(X,Y,Z))).
