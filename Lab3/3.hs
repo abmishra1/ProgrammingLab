135587effc34a9571a208d3609ad77834aab3bd1
Nitin Kedia <nitinkedia7@gmail.com>
2019-11-19 01:20:41 +0530

    Add working code for Q3,4
    TODO: 4x4 sudoku

--- Lab3/3.hs
+++ Lab3/3.hs
@@ -1,3 +1,7 @@
+
+import Data.IORef
+import System.Random
+import System.IO.Unsafe
 import Data.List.Split
 
 teams = ["BS", "CM", "CH", "CV", "CS", "DS", "EE", "HU", "MA", "ME", "PH", "ST"]
@@ -9,6 +13,15 @@ instance Show Time where
 data Match = Empty | Match { team1 :: String, team2 :: String, date :: Int, time :: Time }
 instance Show Match where
     show (Match team1 team2 date time) = team1 ++ " vs " ++ team2 ++ " " ++ show date ++ " " ++ show time
+
+allFixtures :: IORef [Match]
+{-# NOINLINE allFixtures #-}
+allFixtures = unsafePerformIO (newIORef [])
+
+getAllFixtures :: IO [Match]
+getAllFixtures = do readIORef allFixtures
+setAllFixtures :: [Match] -> IO()
+setAllFixtures newFixtures = do writeIORef allFixtures newFixtures
 
 validateTime :: String -> Bool
 validateTime time = do
@@ -29,9 +42,9 @@ lessThan (Time hh1 mm1) (Time hh2 mm2) =
 validateDate :: Int -> Bool
 validateDate date = date >= 1 && date <= 31
 
-genFixture :: [String] -> [Match]
-genFixture [] = []
-genFixture (team1:team2:remTeams) = do
+genFixture' :: [String] -> [Match]
+genFixture' [] = []
+genFixture' (team1:team2:remTeams) = do
     let l = length remTeams
     let d = subtract (l `div` 4) 3
     let t = if ((l `div` 2 ) `mod` 2 == 1) then Time {hh = 9, mm = 30} else Time {hh = 19, mm = 30}
@@ -40,16 +53,33 @@ genFixture (team1:team2:remTeams) = do
         team2 = team2,
         date = d,
         time = t
-    } : genFixture remTeams
+    } : genFixture' remTeams
+
+-- genFixture :: IO()
+genFixture = do
+    steams <- shuffle teams
+    setAllFixtures (genFixture' steams)
+
+-- shuffle :: [String] -> [String]
+shuffle teams = 
+    if (length teams < 2) then return teams 
+    else do
+        i <- System.Random.randomRIO (0, length(teams)-1)
+        r <- shuffle (take i teams ++ drop (i+1) teams)
+        return (teams!!i : r)
 
 fixture' :: String -> [Match] -> [Match]
 fixture' arg allFixtures =
     filter (\(Match team1 team2 _ _) -> team1 == arg || team2 == arg) allFixtures
 
-fixture :: String -> [Match] -> IO()
-fixture arg allFixtures
-    | arg == "all" = printCustom allFixtures
-    | elem arg teams = printCustom (fixture' arg allFixtures)
+fixture :: String -> IO()
+fixture arg
+    | arg == "all" = do
+        allFixtures <- getAllFixtures
+        printCustom allFixtures
+    | elem arg teams = do
+        allFixtures <- getAllFixtures
+        printCustom (fixture' arg allFixtures)
     | otherwise = putStrLn ("Invalid argument " ++ arg)
 
 nextMatch' :: Int -> Time -> [Match] -> [Match]
@@ -61,11 +91,12 @@ nextMatch' givenDate givenTime allFixtures
         else nextMatch' givenDate givenTime nextMatches
         where (curMatch : nextMatches) = allFixtures
 
-nextMatch :: Int -> String -> [Match] -> IO()
-nextMatch givenDate givenTime allFixtures
+nextMatch :: Int -> String -> IO()
+nextMatch givenDate givenTime
     | not (validateDate givenDate) = putStrLn "Invalid Date"
     | not (validateTime givenTime) = putStrLn "Invalid Time"
     | otherwise = do
+        allFixtures <- getAllFixtures
         printCustom (nextMatch' givenDate (getTime givenTime) allFixtures)
 
 printCustom :: [Match] -> IO()
--- Lab3/4.hs
+++ Lab3/4.hs
@@ -4,24 +4,69 @@ type Value = Int
 type Position = (Int, Int)
 type Sudoku = Array Position Value
 
-readSudoku :: String -> [[Integer]]
-readSudoku input = read (input)
+-- readRow :: String -> [Value]
+-- readRow input = read ("[" ++ input ++ "]")
 
+-- readSudoku :: Int -> [[Value]]
+-- readSudoku 0 = [[]]
+-- readSudoku n = do
+--         rowVal <- getLine
+--         let row = (readRow rowVal)
+--         [row] : (readSudoku (n-1))
 
-main = do
-    putStrLn "Enter the sudoku in  a list of list format seperated by commas"
-    input <- getLine 
-    let sudoku = (createSudoku input)
-    let solved = (solve sudoku)
-    printSudoku solved
+-- main = do
+--     putStrLn "Enter the sudoku in  a grid format seperated by commas"
+--     let input = readSudoku 9
+--     let sudoku = array((0,0),(8,8)) $ (createSudoku input)
+--     let solved = (solve sudoku)
+--     printSudoku solved
+
+wrapper :: [[Value]] -> IO()
+wrapper values =
+    if checkAllValues values then
+        do 
+        let sudoku = array((0,0),(8,8)) $ (createSudoku values)
+        let pos = range((0,0),(8,8))
+        if checkRepeat sudoku  pos then
+            do
+            let solved = (solve sudoku)
+            printSudoku solved
+        else
+            putStrLn "Repetition in given values"
+    else
+        putStrLn "Invalid Value present"
+
+checkRepeat :: Sudoku ->[Position]-> Bool
+checkRepeat sudoku [] = True
+checkRepeat sudoku (pos:xs)
+        | (sudoku ! pos == 0) = checkRepeat sudoku xs
+        | otherwise = do
+                    let val = sudoku ! pos
+                    let sudokuTemp = sudoku // [(pos,0)]
+                    if (isValueValid sudokuTemp pos val) then 
+                        checkRepeat sudoku xs
+                    else
+                        False
+
+checkAllValues :: [[Value]] -> Bool
+checkAllValues [] = True
+checkAllValues (x:xs)
+            | (checkRowValues x) = checkAllValues xs
+            | otherwise = False
+
+checkRowValues :: [Value] -> Bool
+checkRowValues [] = True
+checkRowValues (x:xs)
+            | (x>=0 && x<=9 ) = checkRowValues xs
+            | otherwise = False
 
 printSudoku :: Maybe Sudoku -> IO ()
 printSudoku Nothing  = putStrLn "No solution"
-printSudoku (Just b) = mapM_ putStrLn [([show $ b ! (row,col) | col<-[0..8]]) | row <- [0..8]]
+printSudoku (Just b) = mapM_ putStrLn [ show $ [ b ! pos | pos <- range ((row,0),(row,8))]  | row <- [0..8]]
 
 
 solve :: Sudoku -> Maybe Sudoku
-solve = headOrNothing . solutions
+solve = headOrNothing . solveSudoku
 
 headOrNothing :: [a] -> Maybe a
 headOrNothing []     = Nothing
@@ -29,7 +74,7 @@ headOrNothing (x:xs) = Just x
 
 
 solveSudoku :: Sudoku -> [Sudoku]
-solveSudoku sudoku = solveOnePos $( getEmptyPosition sudoku ) sudoku
+solveSudoku sudoku = solveOnePos (getEmptyPosition sudoku) sudoku
 
 createSudoku :: [[Value]] -> [(Position, Value)]
 createSudoku  values = concatMap createRow $ (zip [0..8] values)
@@ -42,18 +87,18 @@ createSudoku  values = concatMap createRow $ (zip [0..8] values)
 
 solveOnePos :: [Position] -> Sudoku -> [Sudoku]
 solveOnePos [] sudoku = [sudoku]
-solveOnePos (curPos:remPositions) soduko =
+solveOnePos (curPos:remPositions) sudoku =
         -- substitute valid values at curPos
          concatMap (solveOnePos remPositions) validSudokus
     where
-        validValues = filter (isValueValid sudoku curPos [1..9])
+        validValues = filter (isValueValid sudoku curPos) [1..9]
         validSudokus = map (updatePos sudoku curPos) validValues
     
 updatePos :: Sudoku -> Position -> Value -> Sudoku
-updatePos sudoku pos val = (sudoku // [pos, val])
+updatePos sudoku pos val = (sudoku // [(pos, val)])
         
 isValueValid :: Sudoku -> Position -> Value -> Bool
-isValueValid val pos@(row, col) sudoku =
+isValueValid sudoku pos@(row, col) val  =
     canBeUsedInRow sudoku row val
     && canBeUsedInCol sudoku col val
     && canBeUsedInGrid sudoku pos val
